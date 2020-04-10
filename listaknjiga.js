9358f33b3da5266580b71946dad3e4959c284477
Nikola Jovicic <nikolajovicic.nj@gmail.com>
2020-04-10 14:44:53 +0200

    dotati fajlovi

---
+++ listaknjiga.js
@@ -0,0 +1,27 @@
+exports.ListaKnjiga = [
+{
+    'id':'1',
+    'naziv':'Uznemireni ljudi',
+    'autor':'Fredrik Bakman'
+},
+{
+    'id':'2',
+    'naziv':'Ono sve što znaš o meni',
+    'autor':'Mirjana Bobić Mojsilović'
+},
+{
+    'id':'3',
+    'naziv':'Časovničareva kći',
+    'autor':'Kejt Morton'
+},
+{
+    'id':'4',
+    'naziv':'Moravska noć',
+    'autor':'Peter Handke'
+},
+{
+    'id':'5',
+    'naziv':'Čovek po imenu Uve',
+    'autor':'Fredrik Bakman'
+}
+];
